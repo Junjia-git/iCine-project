93f7dbdc0969fae8552a3ca04c7fc2c18caad3d2
Junjia-git <59793918+Junjia-git@users.noreply.github.com>
2020-03-21 17:35:45 +0000

    Final version

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6u8yr=zi(6kw+b@=684vmp!uu!2zis^pep_9a#0(jmb(2f5@x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['JayHuang.pythonanywhere.com','127.0.0.1']
 
